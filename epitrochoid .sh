217c9dd6a1958208bc088c91f657988587795404
[Dillon Reading] <dillreading@gmail.com>
2026-01-15 14:42:30 -0700

    first commit

---
+++ epitrochoid .sh
@@ -0,0 +1,2 @@
+# An example of how one might run epitrochoid.ps in Linux 
+gs -f epitrochoid.ps
---
+++ epitrochoid.bat
@@ -0,0 +1,2 @@
+# Example of one might run epitrochoid.ps on Windows, but will probably need to edit the path
+"C:\Program Files\gs\gs9.52\bin\gswin64.exe" "-IC:\Program Files\gs\gs9.52\lib;C:\Program Files\gs\gs9.52\..\fonts" -f epitrochioid.ps
